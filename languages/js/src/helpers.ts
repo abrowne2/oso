335cd4dc394709f495b9b43acd7785b280c2ead8
abrowne2 <adamo.browne@gmail.com>
2023-09-12 17:32:04 -0400

    Oso: Remove getColorDepth to fix node builtins for Bun on ubuntu

--- languages/js/src/helpers.ts
+++ languages/js/src/helpers.ts
@@ -302,16 +302,16 @@ export function readFile(file: string): Promise<string> {
 let RESET = '';
 let FG_BLUE = '';
 let FG_RED = '';
-if (
-  typeof process?.stdout?.getColorDepth === 'function' &&
-  process.stdout.getColorDepth() >= 4 &&
-  typeof process?.stderr?.getColorDepth === 'function' &&
-  process.stderr.getColorDepth() >= 4
-) {
-  RESET = '\x1b[0m';
-  FG_BLUE = '\x1b[34m';
-  FG_RED = '\x1b[31m';
-}
+// if (
+//   typeof process?.stdout?.getColorDepth === 'function' &&
+//   process.stdout.getColorDepth() >= 4 &&
+//   typeof process?.stderr?.getColorDepth === 'function' &&
+//   process.stderr.getColorDepth() >= 4
+// ) {
+//   RESET = '\x1b[0m';
+//   FG_BLUE = '\x1b[34m';
+//   FG_RED = '\x1b[31m';
+// }
 /** @internal */
 export const PROMPT = FG_BLUE + 'query> ' + RESET;
 
